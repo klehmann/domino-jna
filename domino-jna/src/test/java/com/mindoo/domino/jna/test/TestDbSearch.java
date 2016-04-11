6d7d8df723cf988aa78d450ede3ae1faf135411c
Karsten Lehmann <karsten.lehmann@mindoo.de>
2016-04-11 11:19:44 +0200

    Added documentation to db search testcase

--- domino-jna/src/test/java/com/mindoo/domino/jna/test/TestDbSearch.java
+++ domino-jna/src/test/java/com/mindoo/domino/jna/test/TestDbSearch.java
@@ -35,16 +35,25 @@ public class TestDbSearch extends BaseJNATestClass {
 				NotesDatabase dbData = getFakeNamesDb();
 				final Database dbLegacyAPI = session.getDatabase(dbData.getServer(), dbData.getRelativeFilePath());
 				
+				//example prefix string to read some data
 				final String searchPrefix = "Tyso";
+				//example view title returned by @ViewTitle when formula is evaluated
 				final String viewTitle = "MyView";
 				
+				//use DEFAULT statements to add our own field values to the summary buffer data
+				//to be returned in the search callback
 				String formula = "DEFAULT _docLength := @DocLength;\n" + 
 				"DEFAULT _viewTitle := @ViewTitle;\n" +
 				"SELECT Form=\"Person\" & @Begins(Lastname;\""+searchPrefix+"\")";
+				
 				EnumSet<Search> searchFlags = EnumSet.of(Search.NOABSTRACTS,
 						Search.SESSION_USERNAME, Search.SUMMARY);
 				
 				short noteClassMask = NotesCAPI.NOTE_CLASS_DOCUMENT;
+				
+				long t0=System.currentTimeMillis();
+				System.out.println("Running database search with formula: "+formula);
+				final int[] cnt = new int[1];
 				
 				dbData.search(formula, viewTitle, searchFlags, noteClassMask, null, new ISearchCallback() {
 
@@ -52,11 +61,15 @@ public class TestDbSearch extends BaseJNATestClass {
 					public void noteFound(NotesDatabase parentDb, int noteId, short noteClass, NotesTimeDate dbCreated,
 							NotesTimeDate noteModified, ItemTableData summaryBufferData) {
 						
+						cnt[0]++;
 						Map<String,Object> summaryData = summaryBufferData.asMap();
 						Assert.assertTrue("Default value computed", summaryData.containsKey("_docLength"));
 						Assert.assertTrue("@ViewTitle returns correct value", viewTitle.equals(summaryData.get("_viewTitle")));
 						
+						System.out.println("#"+cnt[0]+"\tnoteid="+noteId+", noteclass="+noteClass+", dbCreated="+dbCreated+", noteModified="+noteModified+", summary buffer="+summaryData);
+						
 						try {
+							//load document from the database to verify that it really matches our formula
 							Document doc = dbLegacyAPI.getDocumentByID(Integer.toString(noteId, 16));
 							String lastName = doc.getItemValueString("Lastname");
 							doc.recycle();
@@ -65,8 +78,9 @@ public class TestDbSearch extends BaseJNATestClass {
 							e.printStackTrace();
 						}
 					}
-					
 				});
+				long t1=System.currentTimeMillis();
+				System.out.println("Database search done after "+(t1-t0)+"ms. "+cnt[0]+" documents found and processed");
 				
 				return null;
 			}
