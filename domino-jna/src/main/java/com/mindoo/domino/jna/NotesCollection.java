fbc74c7b2be572f0d670f7997c51842c007cfa15
Karsten Lehmann <karsten.lehmann@mindoo.de>
2017-08-02 10:14:05 +0200

    Make sure to return a NotesIDTable when FTSearch.RET_IDTABLE is used
    FTSearch seems not to return a table handle if there is no search
    result.

--- domino-jna/src/main/java/com/mindoo/domino/jna/NotesCollection.java
+++ domino-jna/src/main/java/com/mindoo/domino/jna/NotesCollection.java
@@ -755,7 +755,17 @@ public class NotesCollection implements IRecyclableNotesObject {
 			}
 			NotesErrorUtils.checkResult(result);
 			
-			return new SearchResult(rethResults.getValue()==0 ? null : new NotesIDTable(rethResults.getValue(), false), retNumDocs.getValue());
+			NotesIDTable resultsIdTable = null;
+
+			long hResults = rethResults.getValue();
+			if (hResults!=0) {
+				resultsIdTable = new NotesIDTable(rethResults.getValue(), false);
+			}
+			if (options.contains(FTSearch.RET_IDTABLE) && resultsIdTable==null) {
+				resultsIdTable = new NotesIDTable();
+			}
+
+			return new SearchResult(resultsIdTable, retNumDocs.getValue());
 		}
 		else {
 			IntByReference rethResults = new IntByReference();
@@ -777,7 +787,17 @@ public class NotesCollection implements IRecyclableNotesObject {
 			}
 			NotesErrorUtils.checkResult(result);
 			
-			return new SearchResult(rethResults.getValue()==0 ? null : new NotesIDTable(rethResults.getValue(), false), retNumDocs.getValue());
+			NotesIDTable resultsIdTable = null;
+			
+			int hResults = rethResults.getValue();
+			if (hResults!=0) {
+				resultsIdTable = new NotesIDTable(rethResults.getValue(), false);
+			}
+			if (options.contains(FTSearch.RET_IDTABLE) && resultsIdTable==null) {
+				resultsIdTable = new NotesIDTable();
+			}
+
+			return new SearchResult(resultsIdTable, retNumDocs.getValue());
 		}
 	}
 	
