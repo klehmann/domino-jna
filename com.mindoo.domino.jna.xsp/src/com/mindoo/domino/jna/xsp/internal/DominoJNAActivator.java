fd96f851dcb470dd3e5a912b84bb42384f9f291a
Karsten Lehmann <karsten.lehmann@mindoo.de>
2018-01-13 00:22:35 +0100

    Work around dll version conflicts w/ jnidispatch.dll in Domino progdir

--- com.mindoo.domino.jna.xsp/src/com/mindoo/domino/jna/xsp/internal/DominoJNAActivator.java
+++ com.mindoo.domino.jna.xsp/src/com/mindoo/domino/jna/xsp/internal/DominoJNAActivator.java
@@ -1,5 +1,8 @@
 package com.mindoo.domino.jna.xsp.internal;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
@@ -7,11 +10,37 @@ public class DominoJNAActivator extends Plugin {
 	public static final String PLUGIN_ID = "com.mindoo.domino.jna.xsp";
 	
 	private static DominoJNAActivator plugin;
+	private static Class m_jnaNativeClazz;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		if (m_jnaNativeClazz==null) {
+			m_jnaNativeClazz = AccessController.doPrivileged(new PrivilegedAction<Class>() {
+
+				@Override
+				public Class run() {
+					//enforce using the extracted JNA .dll/.so file instead of what we find on the PATH
+					System.setProperty("jna.nosys", "true");
+					//change the library name from the default "jnidispatch" to our own name, so that
+					//JNA does not load an jnidispatcher.dll from the Server's program directory
+					String oldLibName = System.getProperty("jna.boot.library.name");
+					System.setProperty("jna.boot.library.name", "dominojnadispatch");
+					try {
+						//loading the Native class runs its static code that extracts and loads the jna dll
+						return DominoJNAActivator.class.forName("com.sun.jna.Native");
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+					finally {
+						if (oldLibName!=null)
+							System.setProperty("jna.boot.library.name",oldLibName);
+					}
+					return null;
+				}
+			});
+		}
 	}
 	
 	@Override
