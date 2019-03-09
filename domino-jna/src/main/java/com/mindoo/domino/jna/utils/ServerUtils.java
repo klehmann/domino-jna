f979ffc8ffbfbe0e077504d2d96ce2928b424081
Karsten Lehmann <karsten.lehmann@mindoo.de>
2019-03-09 17:13:25 +0100

    New function to send server console commands

--- domino-jna/src/main/java/com/mindoo/domino/jna/utils/ServerUtils.java
+++ domino-jna/src/main/java/com/mindoo/domino/jna/utils/ServerUtils.java
@@ -53,21 +53,21 @@ public class ServerUtils {
 	@SuppressWarnings("unchecked")
 	public static List<String> getServerClusterMates(String serverName, ClusterLookup lookupMode) {
 		Memory serverNameCanonical = serverName==null ? null : NotesStringUtils.toLMBCS(NotesNamingUtils.toCanonicalName(serverName), true);
-		
+
 		short result;
-		
+
 		if (PlatformUtils.is64Bit()) {
 			LongByReference phList = new LongByReference();
-			
+
 			result = NotesNativeAPI64.get().NSGetServerClusterMates(serverNameCanonical, lookupMode==null ? 0 : lookupMode.getValue(), phList);
 			if (result == 2078) // "No cluster mates found"
 				return Collections.emptyList();
 			NotesErrorUtils.checkResult(result);
-			
+
 			long hList = phList.getValue();
 			if (hList==0)
 				return Collections.emptyList();
-			
+
 			Pointer pList = Mem64.OSLockObject(hList);
 			try {
 				@SuppressWarnings("rawtypes")
@@ -82,12 +82,12 @@ public class ServerUtils {
 		}
 		else {
 			IntByReference phList = new IntByReference();
-			
+
 			result = NotesNativeAPI32.get().NSGetServerClusterMates(serverNameCanonical, lookupMode==null ? 0 : lookupMode.getValue(), phList);
 			if (result == 2078) // "No cluster mates found"
 				return Collections.emptyList();
 			NotesErrorUtils.checkResult(result);
-			
+
 			int hList = phList.getValue();
 			if (hList==0)
 				return Collections.emptyList();
@@ -102,6 +102,77 @@ public class ServerUtils {
 				Mem32.OSUnlockObject(hList);
 				result = Mem32.OSMemFree(hList);
 				NotesErrorUtils.checkResult(result);
+			}
+		}
+	}
+
+	/**
+	 * This function is used to issue a console command to a server from an API program
+	 * or from an API server add-in program.<br>
+	 * <br>
+	 * If you do not have remote access to the server an error will be returned.<br>
+	 * To have remote access to a server, you must be listed in the Server Document
+	 * Administrators field or in the Admin variable in the server's notes.ini.<br>
+	 * <br>
+	 * NOTE: If you use this function to shut down a server (by entering the "exit" or
+	 * "quit" commands), you may receive an error code of "Server not responding"
+	 * or "Remote system no longer responding".<br>
+	 * <br>
+	 * Assuming that the server was active when you issued the command, these errors
+	 * usually mean that your command was successful (the server shuts down before it
+	 * can return a meaningful response).<br>
+	 * <br>
+	 * NOTE:  This function will return NOERROR if the server's disk is full, but
+	 * the returned response buffer will be 0-length. 
+	 * 
+	 * @param server name of the server that is to receive the console command
+	 * @param command  text command to send to the server's console
+	 * @return returned server's response
+	 */
+	public static String sendConsoleCommand(String server, String command) {
+		Memory serverMem = NotesStringUtils.toLMBCS(server, true);
+		Memory commandMem = NotesStringUtils.toLMBCS(command, true);
+
+		if (PlatformUtils.is64Bit()) {
+			LongByReference hResponseText = new LongByReference();
+
+			short result = NotesNativeAPI64.get().NSFRemoteConsole(serverMem, commandMem, hResponseText);
+			NotesErrorUtils.checkResult(result);
+
+			long hResponseTextValue = hResponseText.getValue();
+			if (hResponseTextValue==0) {
+				return "";
+			}
+
+			Pointer ptr = Mem64.OSLockObject(hResponseTextValue);
+			try {
+				String txt = NotesStringUtils.fromLMBCS(ptr, -1);
+				return txt;
+			}
+			finally {
+				Mem64.OSUnlockObject(hResponseTextValue);
+				Mem64.OSMemFree(hResponseTextValue);
+			}
+		}
+		else {
+			IntByReference hResponseText = new IntByReference();
+
+			short result = NotesNativeAPI32.get().NSFRemoteConsole(serverMem, commandMem, hResponseText);
+			NotesErrorUtils.checkResult(result);
+
+			int hResponseTextValue = hResponseText.getValue();
+			if (hResponseTextValue==0) {
+				return "";
+			}
+
+			Pointer ptr = Mem32.OSLockObject(hResponseTextValue);
+			try {
+				String txt = NotesStringUtils.fromLMBCS(ptr, -1);
+				return txt;
+			}
+			finally {
+				Mem32.OSUnlockObject(hResponseTextValue);
+				Mem32.OSMemFree(hResponseTextValue);
 			}
 		}
 	}
