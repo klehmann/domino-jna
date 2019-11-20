441e01632a8124e43bced5b252ed9ced4d5e29ee
Karsten Lehmann <karsten.lehmann@mindoo.de>
2019-11-20 10:15:50 +0100

    Added testcase that returns view search results sorted by creation/modified date

--- domino-jna/src/test/java/com/mindoo/domino/jna/test/TestViewTraversalWithFTSearch.java
+++ domino-jna/src/test/java/com/mindoo/domino/jna/test/TestViewTraversalWithFTSearch.java
@@ -10,10 +10,12 @@ import com.mindoo.domino.jna.NotesCollection.Direction;
 import com.mindoo.domino.jna.NotesDatabase;
 import com.mindoo.domino.jna.NotesFTSearchResult;
 import com.mindoo.domino.jna.NotesIDTable;
+import com.mindoo.domino.jna.NotesNote;
 import com.mindoo.domino.jna.NotesViewEntryData;
 import com.mindoo.domino.jna.constants.FTSearch;
 import com.mindoo.domino.jna.constants.Navigate;
 import com.mindoo.domino.jna.constants.ReadMask;
+import com.mindoo.domino.jna.gc.NotesGC;
 
 import lotus.domino.Session;
 
@@ -110,4 +112,115 @@ public class TestViewTraversalWithFTSearch extends BaseJNATestClass {
 		});
 	}
 
+	/**
+	 * Runs a FT search and applies the result onto a NotesCollection index. When navigating in
+	 * the view, the results
+	 */
+	@Test
+	public void testReadSearchResultsFromViewViaHitNav() {
+		runWithSession(new IDominoCallable<Object>() {
+
+			@Override
+			public Object call(Session session) throws Exception {
+				NotesGC.setDebugLoggingEnabled(false);
+				
+				System.out.println("Starting testcase testReadSearchResultsFromViewViaHitNav:");
+
+				NotesDatabase dbData = getFakeNamesDb();
+				
+				//view should be non-permuted => documents should not exist in the view multiple times, otherwise
+				//we would get duplicates in the result data and there's a chance that getAllEntries runs
+				//in an infinite loop, because it's working position based and the positions returned from
+				//the C API are not correct for duplicate occurences (always returns the position of the
+				//first doc occurence, already reported to HCL dev)
+				NotesCollection companiesView = dbData.openCollectionByName("Companies");
+				
+				//resort view by company name
+				companiesView.resortView("companyname", Direction.Ascending);
+				
+				String ftQuery = "[lastname]=Hill OR [lastname]=Potter";
+
+				//run database FT index search, restricted to the note ids of the view
+				EnumSet<FTSearch> searchOptions = EnumSet.noneOf(FTSearch.class);
+				
+				//optional search options:
+				searchOptions.add(FTSearch.FUZZY); // do a fuzzy search
+				searchOptions.add(FTSearch.STEM_WORDS); // stem words
+				searchOptions.add(FTSearch.THESAURUS_WORDS); // use thesaurus
+				
+				// just return number of results in viewSearchResult.getNumDocs();
+				// if set, navigating the view via NEXT_HIT returns no results
+				// searchOptions.add(FTSearch.NUMDOCS_ONLY);
+				
+				// view FT search supports two sort modes, by creation date or by last modified (in this file):
+				// searchOptions.add(FTSearch.SORT_DATE_CREATED);
+				// searchOptions.add(FTSearch.SORT_DATE);
+				// 
+				// if both sort options are not set and searchOptions contains FTSearch.SCORES, the results
+				// are sorted by relevance
+				
+				// we select to sort by last modified:
+				searchOptions.add(FTSearch.SORT_DATE_MODIFIED);
+
+				// by default, the results are sorted by creation date or last modified date in descending order;
+				// use SORT_ASCEND to reverse sort order:
+				searchOptions.add(FTSearch.SORT_ASCEND);
+
+				NotesFTSearchResult viewSearchResult = companiesView.ftSearch(ftQuery, 0, searchOptions);
+				int numHits = viewSearchResult.getNumHits();
+				System.out.println("Number of FT results found: "+numHits);
+				
+				//paging offset/count in view:
+				int offset = 0;
+				int count = 1000; // Integer.MAX_VALUE;
+				
+				// amount of entries to be read with one NIFReadEntries call, should be higher if
+				// EntriesAsListCallback.isAccepted is overridden and entries are skipped in code 
+				int preloadEntryCount = count;
+				
+				System.out.println("Reading max. "+count+" entries starting at offset "+offset);
+				System.out.println();
+
+				EnumSet<ReadMask> readMask = EnumSet.of(ReadMask.NOTEID, ReadMask.SUMMARYVALUES);	// not specifying ReadMask.INDEXPOSITION here like in the other test case, because
+																									// the returned data would just return increasing position numbers starting with 1
+
+				
+				// if possible do not do this to get the total count; expensive operation that needs to traverse the
+				// entire view index to count selected view entries
+				//
+				// int totalMatches = companiesView.getEntryCount(Navigate.NEXT_SELECTED);
+				// System.out.println(totalMatches+" docs in the view match the FT query");
+				
+				List<NotesViewEntryData> entries = companiesView.getAllEntries("0", 1+offset, EnumSet.of(Navigate.NEXT_HIT),
+						preloadEntryCount, readMask, new NotesCollection.EntriesAsListCallback(count));
+				
+
+				System.out.println("====================================================");
+				if (searchOptions.contains(FTSearch.SORT_DATE_MODIFIED)) {
+					System.out.println("Reading search hits sorted by last modified in "+
+							(searchOptions.contains(FTSearch.SORT_ASCEND) ? "ascending " : "descending ") + "order");
+				}
+				else if (searchOptions.contains(FTSearch.SORT_DATE_CREATED)) {
+					System.out.println("Reading search hits sorted by creation date in "+
+							(searchOptions.contains(FTSearch.SORT_ASCEND) ? "ascending " : "descending ") + "order");
+				}
+				System.out.println("====================================================");
+
+				int idx=0;
+				for (NotesViewEntryData currEntry : entries) {
+					idx++;
+					NotesNote note = dbData.openNoteById(currEntry.getNoteId());
+					
+					System.out.println("#"+idx+
+							"\tcreated="+note.getCreationDateAsTimeDate()+
+							", modifiedinfile="+note.getOID().getSequenceTime() +
+							", noteid="+currEntry.getNoteId()+", columns="+currEntry.getColumnDataAsMap());
+					note.recycle();
+				}
+
+				System.out.println("Done.");
+				return null;
+			}
+		});
+	}
 }
