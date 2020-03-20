99eb2365189a31eb7cd529700d11a8883dfd1c7d
Karsten Lehmann <karsten.lehmann@mindoo.de>
2020-03-20 12:48:42 +0100

    Added methods to scan for attachment names in richtext items

--- domino-jna/src/main/java/com/mindoo/domino/jna/richtext/RichTextUtils.java
+++ domino-jna/src/main/java/com/mindoo/domino/jna/richtext/RichTextUtils.java
@@ -1,11 +1,22 @@
 package com.mindoo.domino.jna.richtext;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+import com.mindoo.domino.jna.NotesItem;
+import com.mindoo.domino.jna.NotesNote;
 import com.mindoo.domino.jna.constants.CDRecordType;
 import com.mindoo.domino.jna.internal.FieldPropAdaptable;
+import com.mindoo.domino.jna.internal.NotesConstants;
 import com.mindoo.domino.jna.richtext.IRichTextNavigator.RichTextNavPosition;
+import com.mindoo.domino.jna.utils.NotesStringUtils;
 import com.sun.jna.Memory;
 
 /**
@@ -102,5 +113,119 @@ public class RichTextUtils {
 		return fields;
 	}
 	
+	/**
+	 * Scans all richtext items of a note to find all attachment icons and
+	 * return the attachment names hashed by richtext item name
+	 * 
+	 * @param note note to scan
+	 * @return map with item name as key (case insensitive) and attachment names as value in order of appearance
+	 */
+	public static Map<String,LinkedHashSet<String>> collectAllAttachmentNamesByField(NotesNote note) {
+		Map<String,LinkedHashSet<String>> result = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+		
+		Set<String> allRichtextItemNames = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+		
+		note.getItems((item,loop) -> {
+			if (item.getType() == NotesItem.TYPE_COMPOSITE) {
+				allRichtextItemNames.add(item.getName());
+			}
+		});
+		
+		for (String currItemName : allRichtextItemNames) {
+			IRichTextNavigator rtNav = note.getRichtextNavigator(currItemName);
+			LinkedHashSet<String> attachmentNamesInItem = collectAttachmentNames(rtNav);
+			result.put(currItemName, attachmentNamesInItem);
+		}
+		
+		return result;
+	}
 	
+	/**
+	 * Traverses the richtext CD records and collects the filenames of all
+	 * attachment icons. Restores the
+	 * current {@link RichTextNavPosition} in the richtext item if not
+	 * null.
+	 * 
+	 * @param note note to scan
+	 * @param itemName name of richtext item
+	 * @return attachment names in order of appearance
+	 */
+	public static LinkedHashSet<String> collectAttachmentNames(NotesNote note, String itemName) {
+		IRichTextNavigator rtNav = note.getRichtextNavigator(itemName);
+		if (rtNav!=null) {
+			return collectAttachmentNames(rtNav);
+		}
+		else {
+			return new LinkedHashSet<>();
+		}
+	}
+	
+	/**
+	 * Traverses the richtext CD records and collects the filenames of all
+	 * attachment icons. Restores the
+	 * current {@link RichTextNavPosition} in the richtext item if not
+	 * null.
+	 * 
+	 * @param rtNav richtext navigator
+	 * @return attachment names in order of appearance
+	 */
+	public static LinkedHashSet<String> collectAttachmentNames(IRichTextNavigator rtNav) {
+		LinkedHashSet<String> attNames = new LinkedHashSet<>();
+		
+		if (rtNav==null) {
+			return attNames;
+		}
+		
+		RichTextNavPosition oldPos = rtNav.getCurrentRecordPosition();
+		
+		if (rtNav.gotoFirst()) {
+			do {
+				if (CDRecordType.BEGIN.getConstant() == rtNav.getCurrentRecordTypeAsShort()) {
+//					typedef struct {
+//						   BSIG Header;    /* Signature and length of this record */
+//						   WORD Version;		
+//						   WORD Signature; /* Signature of record begin is for */
+//						} CDBEGINRECORD;
+					Memory beginDataBuf = rtNav.getCurrentRecordData();
+					
+					int version = beginDataBuf.getShort(0);
+					int signature = beginDataBuf.share(2).getShort(0);
+					
+					if (signature == NotesConstants.SIG_CD_V4HOTSPOTBEGIN) {
+						RichTextNavPosition savedPos = rtNav.getCurrentRecordPosition();
+						if (rtNav.gotoNext()) {
+							//check what is next
+							if (CDRecordType.HOTSPOTBEGIN.getConstant() == rtNav.getCurrentRecordTypeAsShort()) {
+//								typedef struct {
+//								   WSIG  Header; /* Signature and length of this record */	
+//								   WORD  Type;
+//								   DWORD Flags;
+//								   WORD  DataLength;
+//								   Data follows...
+									/*  if HOTSPOTREC_RUNFLAG_SIGNED, WORD SigLen then SigData follows. */
+//								} CDHOTSPOTBEGIN;
+
+								Memory hotspotRecordDataBuf = rtNav.getCurrentRecordData();
+								
+								short type = hotspotRecordDataBuf.getShort(0);
+								if (type == NotesConstants.HOTSPOTREC_TYPE_FILE) {
+									String uniqueFileName = NotesStringUtils.fromLMBCS(hotspotRecordDataBuf.share(8), -1);
+									attNames.add(uniqueFileName);
+								}
+							}
+						}
+						rtNav.restoreCurrentRecordPosition(savedPos);
+					}
+				}
+			}
+			while (rtNav.gotoNext());
+			
+		}
+		
+		if (oldPos!=null) {
+			rtNav.restoreCurrentRecordPosition(oldPos);
+		}
+		
+		return attNames;
+	}
 }
--- domino-jna/src/test/java/com/mindoo/domino/jna/test/TestRichTextCreationAndReading.java
+++ domino-jna/src/test/java/com/mindoo/domino/jna/test/TestRichTextCreationAndReading.java
@@ -7,6 +7,10 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -19,10 +23,9 @@ import com.mindoo.domino.jna.constants.CDRecordType;
 import com.mindoo.domino.jna.richtext.FontStyle;
 import com.mindoo.domino.jna.richtext.IRichTextNavigator;
 import com.mindoo.domino.jna.richtext.RichTextBuilder;
+import com.mindoo.domino.jna.richtext.RichTextUtils;
 import com.mindoo.domino.jna.richtext.TextStyle;
 import com.mindoo.domino.jna.richtext.TextStyle.Justify;
-import com.mindoo.domino.jna.richtext.conversion.AppendFileHotspotConversion;
-import com.mindoo.domino.jna.richtext.conversion.RemoveFileHotspotConversion;
 import com.mindoo.domino.jna.utils.NotesNamingUtils;
 import com.mindoo.domino.jna.utils.NotesStringUtils;
 
@@ -49,25 +52,25 @@ public class TestRichTextCreationAndReading extends BaseJNATestClass {
 			public Object call(Session session) throws Exception {
 				//sample only works when run against the Notes Client
 				NotesDatabase db = NotesDatabase.openMailDatabase();
-				
+
 				//create a note with some richtext
 				NotesNote note = db.createNote();
 				note.replaceItemValue("Form", "Memo");
-				
-				
+
+
 				// use this syntax to attach a file from disk:
 				// attachFile(String filePathOnDisk, String uniqueFileNameInNote, Compression compression)
 				// note.attachFile("/tmp/testfile.txt", "testfile.txt", Compression.NONE);
-				
+
 				// or use the IAttachmentProducer to generate the file data on the fly:
 				IAttachmentProducer attProducer = new IAttachmentProducer() {
-					
+
 					@Override
 					public void produceAttachment(OutputStream out) throws IOException {
 						String txt = "Hello. This is a testfile.";
 						out.write(txt.getBytes(Charset.forName("UTF-8")));
 					}
-					
+
 					@Override
 					public int getSizeEstimation() {
 						//returning a value here would help allocating the right binary object
@@ -75,57 +78,57 @@ public class TestRichTextCreationAndReading extends BaseJNATestClass {
 						return -1;
 					}
 				};
-				
+
 				NotesTimeDate fileCreated = NotesTimeDate.adjustedFromNow(0, 0, 5, 0, 0, 0);
 				NotesTimeDate fileModified = NotesTimeDate.adjustedFromNow(0, 0, 2, 0, 0, 0);
 				NotesAttachment att = note.attachFile(attProducer, "testfile.txt", fileCreated.toDate(), fileModified.toDate());
-				
+
 				RichTextBuilder rt = note.createRichTextItem("Body");
 				rt.addText("Here is the file:\n");
 				rt.addFileHotspot(att, "captiontext for testfile.txt");
-				
+
 				// use this for more advanced options, e.g. use a different file icon and caption color/text placement:
 				// rt.addFileHotspot(attachment, filenameToDisplay, captionText, captionStyle, captionPos, captionColorRed, captionColorGreen, captionColorBlue, resizeToWidth, resizeToHeight, fileSize, imageData);
-				
+
 				// code richtext item
 				rt.close();
 				// save document
 				note.update();
-				
+
 				System.out.println("Document created with Notes URL Notes://"+
 						NotesNamingUtils.toCommonName(db.getServer())+"/"+
 						db.getReplicaID()+"/0/"+note.getUNID());
-				
+
 				// use this syntax to modify existing richtext items;
 				// these converters rewrite the richtext item only if they find any matches, e.g. a
 				// file icon for the specfied attachment's unique name
-				
+
 				// note.convertRichTextItem("Body", new AppendFileHotspotConversion(att, "filename.txt"));
 				// note.convertRichTextItem("Body", new RemoveFileHotspotConversion(att));
-				
+
 				return null;
 			}
 		});
 	}
-	
+
 	@Test
-	public void testViewMetaData_defaultCollection() {
+	public void testRichtextAddPNG() {
 		runWithSession(new IDominoCallable<Object>() {
 
 			@Override
 			public Object call(Session session) throws Exception {
 				//sample only works when run against the Notes Client
 				NotesDatabase db = NotesDatabase.openMailDatabase();
-				
+
 				//create a note with some richtext
 				NotesNote note = db.createNote();
 				note.replaceItemValue("Form", "Memo");
-				
+
 				RichTextBuilder rt = note.createRichTextItem("Body");
-				
+
 				//add some multiline text
 				rt.addText("Line 1\nLine2\n");
-				
+
 				//add another paragraph with formatted text
 				TextStyle txtStyle = new TextStyle("Test").setAlign(Justify.RIGHT);
 				FontStyle fontStyle = new FontStyle().setItalic(true);
@@ -139,7 +142,7 @@ public class TestRichTextCreationAndReading extends BaseJNATestClass {
 				}
 				URLConnection conn = imgUrl.openConnection();
 				int fileSize = conn.getContentLength();
-				
+
 				InputStream imageStream = conn.getInputStream();
 				try {
 					rt.addImage(fileSize, imageStream);
@@ -147,18 +150,18 @@ public class TestRichTextCreationAndReading extends BaseJNATestClass {
 				finally {
 					imageStream.close();
 				}
-				
+
 				rt.close();
 				note.update();
 				System.out.println("Document created with Notes URL Notes://"+
 						NotesNamingUtils.toCommonName(db.getServer())+"/"+
 						db.getReplicaID()+"/0/"+note.getUNID());
-				
-				
+
+
 				//now read the text using the convenience method
 				String allTxt = note.getRichtextContentAsText("Body");
 				Assert.assertTrue("Text is not empty", allTxt.length()>0);
-				
+
 				//and compare with manual CD record traversal
 				final StringBuilder sb = new StringBuilder();
 				IRichTextNavigator rtNav = note.getRichtextNavigator("Body");
@@ -179,12 +182,148 @@ public class TestRichTextCreationAndReading extends BaseJNATestClass {
 				}
 
 				Assert.assertEquals("Text content matches", allTxt, sb.toString());
-				
+
 				System.out.println("Content read from richtext item:\n"+allTxt);
+				return null;
+			}
+		});
+	}
+
+	@Test
+	public void testRichtextScanForFiles() {
+		runWithSession(new IDominoCallable<Object>() {
+
+			@Override
+			public Object call(Session session) throws Exception {
+				//sample only works when run against the Notes Client
+				NotesDatabase db = NotesDatabase.openMailDatabase();
+
+				//create a note with some richtext
+				NotesNote note = db.createNote();
+				note.replaceItemValue("Form", "Memo");
+
+				LinkedHashSet<String> insertedFieldBodyItem = new LinkedHashSet<>();
+				
+				{
+					//write richtext item 1
+					RichTextBuilder rt = note.createRichTextItem("Body");
+
+					//add some multiline text
+					rt.addText("Line 1\nLine2\n");
+
+					NotesAttachment file1 = note.attachFile(new IAttachmentProducer() {
+
+						@Override
+						public void produceAttachment(OutputStream out) throws IOException {
+							out.write("test".getBytes(Charset.forName("UTF-8")));
+						}
+
+						@Override
+						public int getSizeEstimation() {
+							return -1;
+						}
+					}, "file1.txt", new Date(), new Date());
+
+					rt.addText("More text");
+
+					NotesAttachment file2 = note.attachFile(new IAttachmentProducer() {
+
+						@Override
+						public void produceAttachment(OutputStream out) throws IOException {
+							out.write("test2".getBytes(Charset.forName("UTF-8")));
+						}
+
+						@Override
+						public int getSizeEstimation() {
+							return -1;
+						}
+					}, "file2.txt", new Date(), new Date());
+
+					//insert in reverse order, to test if our scan results get returned in order of appearance
+					rt.addFileHotspot(file2, "file2-dsp.txt");
+					rt.addFileHotspot(file1, "file1-dsp.txt");
+
+					insertedFieldBodyItem.add(file2.getFileName());
+					insertedFieldBodyItem.add(file1.getFileName());
+					
+					rt.close();
+				}
+
+				LinkedHashSet<String> insertedFieldBody2Item = new LinkedHashSet<>();
+
+				{
+					//write richtext item 1
+					RichTextBuilder rt = note.createRichTextItem("Body2");
+
+					NotesAttachment file3 = note.attachFile(new IAttachmentProducer() {
+
+						@Override
+						public void produceAttachment(OutputStream out) throws IOException {
+							out.write("test3".getBytes(Charset.forName("UTF-8")));
+						}
+
+						@Override
+						public int getSizeEstimation() {
+							return -1;
+						}
+					}, "file3.txt", new Date(), new Date());
+
+					rt.addFileHotspot(file3, "file3-dsp.txt");
+					
+					insertedFieldBody2Item.add(file3.getFileName());
+
+					rt.close();
+				}
+
+				// add a file that is not part of a richtext item
+				NotesAttachment file4 = note.attachFile(new IAttachmentProducer() {
+
+					@Override
+					public void produceAttachment(OutputStream out) throws IOException {
+						out.write("test4".getBytes(Charset.forName("UTF-8")));
+					}
+
+					@Override
+					public int getSizeEstimation() {
+						return -1;
+					}
+				}, "file4.txt", new Date(), new Date());
+
+
+				//search for filenames in item body
+				Set<String> fileNamesItem1;
+				{
+					IRichTextNavigator rtNav = note.getRichtextNavigator("Body");
+					fileNamesItem1 = RichTextUtils.collectAttachmentNames(rtNav);
+
+					System.out.println("Files found in richtext: "+fileNamesItem1);
+
+					Assert.assertEquals("Correct files found in body item", insertedFieldBodyItem, fileNamesItem1);
+				}
+
+				//search for filenames in item body2
+				Set<String> fileNamesItem2;
+				{
+					IRichTextNavigator rtNav = note.getRichtextNavigator("Body2");
+					fileNamesItem2 = RichTextUtils.collectAttachmentNames(rtNav);
+
+					System.out.println("Files found in richtext: "+fileNamesItem2);
+
+					Assert.assertEquals("Correct files found in body2 item", insertedFieldBody2Item, fileNamesItem2);
+				}
+				
+				//search for attachment icon filenames in all note items
+				Map<String,LinkedHashSet<String>> attNamesByItem = RichTextUtils.collectAllAttachmentNamesByField(note);
+
+				System.out.println("Found attachments hashed by their richtext item: "+attNamesByItem);
+				
+				Assert.assertEquals("Found attachments in 2 items", 2, attNamesByItem.size());
+				Assert.assertEquals("Attachments in item body found", fileNamesItem1, attNamesByItem.get("bODy"));
+				Assert.assertEquals("Attachments in item body2 found", fileNamesItem2, attNamesByItem.get("bODy2"));
+				
 				return null;
 			}
 		});
 
 	}
-	
 }
