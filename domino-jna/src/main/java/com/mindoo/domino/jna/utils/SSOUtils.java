9689cbd26dcc8c19e596128752e3ae6b70b86293
Karsten Lehmann <karsten.lehmann@mindoo.de>
2021-09-13 11:38:36 +0200

    Added method to validate an SSO token

--- domino-jna/src/main/java/com/mindoo/domino/jna/utils/SSOUtils.java
+++ domino-jna/src/main/java/com/mindoo/domino/jna/utils/SSOUtils.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import com.mindoo.domino.jna.NotesSSOToken;
 import com.mindoo.domino.jna.NotesTimeDate;
+import com.mindoo.domino.jna.errors.INotesErrorConstants;
+import com.mindoo.domino.jna.errors.NotesError;
 import com.mindoo.domino.jna.errors.NotesErrorUtils;
+import com.mindoo.domino.jna.internal.DisposableMemory;
 import com.mindoo.domino.jna.internal.Mem32;
 import com.mindoo.domino.jna.internal.Mem64;
 import com.mindoo.domino.jna.internal.NotesConstants;
+import com.mindoo.domino.jna.internal.NotesNativeAPI;
 import com.mindoo.domino.jna.internal.NotesNativeAPI32;
 import com.mindoo.domino.jna.internal.NotesNativeAPI64;
 import com.mindoo.domino.jna.internal.structs.NotesSSOTokenStruct32;
@@ -294,4 +298,153 @@ public class SSOUtils {
 		}
 	}
 
+	public static class TokenInfo {
+		private String userName;
+		private NotesTimeDate idleTimeout;
+		private NotesTimeDate creationTime;
+		private NotesTimeDate expirationTime;
+		
+		public String getUserName() {
+			return userName;
+		}
+
+		private TokenInfo setUserName(String userName) {
+			this.userName = userName;
+			return this;
+		}
+
+		public NotesTimeDate getIdleTimeout() {
+			return idleTimeout;
+		}
+		
+		private TokenInfo setIdleTimeout(NotesTimeDate renewalTime) {
+			this.idleTimeout = renewalTime;
+			return this;
+		}
+
+		public NotesTimeDate getCreationTime() {
+			return creationTime;
+		}
+
+		private TokenInfo setCreationTime(NotesTimeDate creationTime) {
+			this.creationTime = creationTime;
+			return this;
+		}
+
+		public NotesTimeDate getExpirationTime() {
+			return expirationTime;
+		}
+
+		private TokenInfo setExpirationTime(NotesTimeDate expirationTime) {
+			this.expirationTime = expirationTime;
+			return this;
+		}
+	}
+	
+	public static class TokenExpiredException extends NotesError {
+		private static final long serialVersionUID = -713498735905846965L;
+		
+		private String userName;
+		private NotesTimeDate idleTimeout;
+		private NotesTimeDate creationTime;
+		private NotesTimeDate expirationTime;
+
+		public TokenExpiredException(int id, String msg, String userName,
+				NotesTimeDate creationTime, NotesTimeDate expirationTime, NotesTimeDate idleTimeout) {
+			super(id, msg);
+			this.userName = userName;
+			this.creationTime = creationTime;
+			this.expirationTime = expirationTime;
+			this.idleTimeout = idleTimeout;
+		}
+		
+		public String getUserName() {
+			return userName;
+		}
+		
+		public NotesTimeDate getCreationTime() {
+			return creationTime;
+		}
+
+		public NotesTimeDate getExpirationTime() {
+			return expirationTime;
+		}
+
+		public NotesTimeDate getIdleTimeout() {
+			return idleTimeout;
+		}
+
+	}
+	
+	/**
+	 * Use this function to validate an authentication token for interoperability with Domino protocols
+	 * that support Single Sign-On (IE: HTTP and DIIOP), as well as IBM WebSphere Advanced Server.<br>
+	 * Returns the username encoded in the token as well as the token creation and expiration time.<br>
+	 * The creation time is not returned if the configuration specified is set up for interoperability
+	 * with IBM's WebSphere. Any error returned from this function represents authentication failure.
+	 * For expired tokens, we throw an {@link TokenExpiredException} that provides access to
+	 * the token's username, creation time, expiration time and idle timeout.
+	 * 
+	 * @param orgName Organization that the server belongs to (OPTIONAL: Specify NULL if not using 'Internet Sites' view for configuration)
+	 * @param configName Name of Web SSO Configuration to use.
+	 * @param tokenData token data
+	 * @param enableRenewal  If true, the routine will return the time at which the Token does its IdleTimeout (which may be sooner than the value returned by Expiration if idle timeout is enabled for this LTPA token). Idle timeout can only be enabled for Domino style tokens. The Websphere format does not support them.
+	 * @return token info
+	 * @throws TokenExpiredException if token is expired; exception provides access to username, creation time, expiration time and idle timeout
+	 */
+	public static TokenInfo validateToken(String orgName, String configName, String tokenData,
+			boolean enableRenewal) throws TokenExpiredException {
+		Memory orgNameMem = NotesStringUtils.toLMBCS(orgName, true);
+		Memory configNameMem = NotesStringUtils.toLMBCS(configName, true);
+		Memory tokenDataMem = NotesStringUtils.toLMBCS(tokenData, true);
+		DisposableMemory retUsernameMem = new DisposableMemory(NotesConstants.MAXUSERNAME);
+		retUsernameMem.clear();
+		
+		try {
+			NotesTimeDateStruct.ByReference retCreationTimeStruct = NotesTimeDateStruct.ByReference.newInstanceByReference();
+			NotesTimeDateStruct.ByReference retExpirationTimeStruct = NotesTimeDateStruct.ByReference.newInstanceByReference();
+			NotesTimeDateStruct.ByReference retIdleTimeStruct = NotesTimeDateStruct.ByReference.newInstanceByReference();
+
+			short result = NotesNativeAPI.get().SECTokenValidate(null, orgNameMem, configNameMem, tokenDataMem,
+					retUsernameMem, retCreationTimeStruct, retExpirationTimeStruct,
+					enableRenewal ? NotesConstants.fSECToken_EnableRenewal : 0, retIdleTimeStruct.getPointer());
+			
+			boolean tokenExpired = false;
+			if ((result & NotesConstants.ERR_MASK)==INotesErrorConstants.ERR_LTPA_TOKEN_EXPIRED) { // "Single Sign-On token is expired"
+				tokenExpired = true;
+				//don't check error code, we throw a TokenExpiredException later
+			}
+			else {
+				NotesErrorUtils.checkResult(result);
+			}
+			
+			String retUsername = NotesStringUtils.fromLMBCS(retUsernameMem, -1);
+			
+			retCreationTimeStruct.read();
+			NotesTimeDate retCreationTime = new NotesTimeDate(retCreationTimeStruct.Innards);
+			
+			retExpirationTimeStruct.read();
+			NotesTimeDate retExpirationTime = new NotesTimeDate(retExpirationTimeStruct.Innards);
+			
+			retIdleTimeStruct.read();
+			NotesTimeDate retIdleTime = new NotesTimeDate(retIdleTimeStruct.Innards);
+			
+			if (tokenExpired) {
+				String msg = NotesErrorUtils.errToString(result);
+				throw new TokenExpiredException(result, msg, retUsername,
+						retCreationTime, retExpirationTime, retIdleTime);
+			}
+			else {
+				return new TokenInfo()
+				.setUserName(retUsername)
+				.setCreationTime(retCreationTime)
+				.setExpirationTime(retExpirationTime)
+				.setIdleTimeout(retIdleTime);
+			}
+		}
+		finally {
+			retUsernameMem.dispose();
+		}
+	}
+	
 }
