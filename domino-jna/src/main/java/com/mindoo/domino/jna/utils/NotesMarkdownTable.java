24bafa48eecde9adafc5f8ae7a3d73e8c93a76bc
Karsten Lehmann <karsten.lehmann@mindoo.de>
2022-03-25 23:27:38 +0100

    Custom column support for NotesMarkdownTable

--- domino-jna/src/main/java/com/mindoo/domino/jna/utils/NotesMarkdownTable.java
+++ domino-jna/src/main/java/com/mindoo/domino/jna/utils/NotesMarkdownTable.java
@@ -5,17 +5,17 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UncheckedIOException;
 import java.io.Writer;
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import com.mindoo.domino.jna.NotesCollection;
 import com.mindoo.domino.jna.NotesIDTable;
 import com.mindoo.domino.jna.NotesViewColumn;
 import com.mindoo.domino.jna.NotesViewEntryData;
-import com.mindoo.domino.jna.constants.ReadMask;
 
 /**
  * Utility class to dump entries of a {@link NotesCollection} as a Markdown table.
@@ -25,137 +25,58 @@ import com.mindoo.domino.jna.constants.ReadMask;
  * Basic usage:<br>
  * <br>
  * <code>
- * new NotesMarkdownTable(notesCollection, System.out)<br>
- *  .withPosition(true)<br>
- *  .withNoteId(true)<br>
- *  .withUNID(true)<br>
- *  .withExpandState(true)<br>
- *  .printHeader()<br>
- *  .printRows(entries)<br>
- *  .printFooter();<br>
+ * new NotesMarkdownTable(collection, System.out)<br>
+ * .addColumn(NotesMarkdownTable.EXPANDSTATE)<br>
+ * .addColumn(NotesMarkdownTable.POS)<br>
+ * .addColumn(NotesMarkdownTable.NOTEID)<br>
+ * .addColumn(NotesMarkdownTable.UNID)<br>
+ * .addColumn("CustomVal1", 30, (entry) -> {<br>
+ * &nbsp;&nbsp;return entry.getAsString("lastname", "") + "," +entry.getAsString("firstname", "");<br>
+ *  })<br>
+ * .addAllViewColumns()<br>
+ * .printHeader()<br>
+ * .printRows(entries)<br>
+ * .printFooter();<br>
  *  </code>
  * 
  * @author Karsten Lehmann
  */
 public class NotesMarkdownTable {
 	private NotesCollection m_view;
-	private Map<NotesViewColumn,Integer> m_visibleColumnWidths;
 	private Writer m_writer;
-	private boolean m_withExpandState;
-	private boolean m_withPosition;
-	private boolean m_withNoteId;
-	private boolean m_withUNID;
-	private NotesIDTable m_collapsedList;
-	private boolean m_collapsedListInverted;
-	
+	private List<ColumnInfo> m_customColumns;
+
 	private int m_docEntryCount;
 	private int m_categoryEntryCount;
 	private int m_totalEntryCount;
-	
+
 	/**
+	 * Creates a new instance
 	 * 
-	 * @param view
-	 * @param out
+	 * @param view view to read column infos
+	 * @param out target steam
 	 */
 	public NotesMarkdownTable(NotesCollection view, OutputStream out) {
 		this(view, new OutputStreamWriter(out));
 	}
-	
+
+	/**
+	 * Creates a new instance
+	 * 
+	 * @param view view to read column infos
+	 * @param writer target writer
+	 */
 	public NotesMarkdownTable(NotesCollection view, Writer writer) {
 		m_view = view;
-		m_collapsedList = m_view.getCollapsedList();
-		m_collapsedListInverted = m_collapsedList.isInverted();
-		
+		m_customColumns = new ArrayList<>();
+
 		m_writer = writer;
-		
-		m_visibleColumnWidths = new HashMap<>();
-		for (NotesViewColumn col : m_view.getColumns()) {
-			if (isVisible(col)) {
-				m_visibleColumnWidths.put(col, getWidthInChars(col));
-			}
-		}
 	}
 
-	/**
-	 * Activates a column to display a +/- in case the entry has children and is
-	 * collapsed/expanded. Make sure to read
-	 * the entry data with {@link ReadMask#INDEXCHILDREN} to have values to display.
-	 * 
-	 * @param b true to display
-	 * @return this table
-	 */
-	public NotesMarkdownTable withExpandState(boolean b) {
-		m_withExpandState = b;
-		return this;
-	}
-	
-	/**
-	 * Activates a column to display the entry position. Make sure to read
-	 * the entry data with {@link ReadMask#INDEXPOSITION} to have values to display.
-	 * 
-	 * @param b true to display
-	 * @return this table
-	 */
-	public NotesMarkdownTable withPosition(boolean b) {
-		m_withPosition = b;
-		return this;
-	}
-	
-	/**
-	 * Activates a column to display the entry note id. Make sure to read
-	 * the entry data with {@link ReadMask#NOTEID} to have values to display.
-	 * 
-	 * @param b true to display
-	 * @return this table
-	 */
-	public NotesMarkdownTable withNoteId(boolean b) {
-		m_withNoteId = b;
-		return this;
-	}
-	
-	/**
-	 * Activates a column to display the entry note id. Make sure to read
-	 * the entry data with {@link ReadMask#NOTEUNID} to have values to display.
-	 * 
-	 * @param b true to display
-	 * @return this table
-	 */
-	public NotesMarkdownTable withUNID(boolean b) {
-		m_withUNID = b;
-		return this;
-	}
-
-	private enum ExpandState {NONE, EXPANDED, COLLAPSED};
-	
-	private ExpandState getExpandState(NotesViewEntryData entry) {
-		int noteId = entry.getNoteId();
-		if (noteId==0) {
-			return ExpandState.NONE;
-		}
-		
-		int childCount = entry.getChildCount();
-		if (childCount==0) {
-			return ExpandState.NONE;
-		}
-		
-		NotesIDTable idTable = m_view.getCollapsedList();
-		if (m_collapsedListInverted) {
-			//expand list
-			return idTable.contains(noteId) ? ExpandState.EXPANDED : ExpandState.COLLAPSED;
-		}
-		else {
-			return idTable.contains(noteId) ? ExpandState.COLLAPSED : ExpandState.EXPANDED;
-		}
-	}
-	
-	protected int getWidthInChars(NotesViewColumn col) {
-		return 40;
-	}
-	
 	protected boolean isVisible(NotesViewColumn col) {
 		return true;
 	}
-	
+
 	private String format(String str, int len) {
 		if (str.length() < len) {
 			return StringUtil.pad(str, len, ' ', true);
@@ -164,80 +85,149 @@ public class NotesMarkdownTable {
 			return str.substring(0, len);
 		}
 	}
-	
-	protected int getPositionColumnWidth() {
-		return 20;
+
+	public static class ColumnInfo {
+		private String title;
+		private int width;
+		private Function<NotesViewEntryData,String> fct;
+
+		public ColumnInfo(String title, int width, Function<NotesViewEntryData,String> fct) {
+			this.title = title;
+			this.width = width;
+			this.fct = fct;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public int getWidth() {
+			return width;
+		}
+
+		public Function<NotesViewEntryData, String> getFunction() {
+			return fct;
+		}
+
+	}
+
+	/**
+	 * Adds a computed column to the table
+	 * 
+	 * @param title column title
+	 * @param width column width in characters
+	 * @param fct function to compute the value from a {@link NotesViewEntryData}
+	 */
+	public NotesMarkdownTable addColumn(String title, int width, Function<NotesViewEntryData,String> fct) {
+		ColumnInfo col = new ColumnInfo(title, width, fct);
+		m_customColumns.add(col);
+		return this;
 	}
 	
+	/**
+	 * Adds a computed column to the table, e.g.<br>
+	 * <ul>
+	 * <li>{@link #EXPANDSTATE}</li>
+	 * <li>{@link #POS}</li>
+	 * <li>{@link #NOTEID}</li>
+	 * <li>{@link #UNID}</li>
+	 * 
+	 * @param col column
+	 * @return this table
+	 */
+	public NotesMarkdownTable addColumn(ColumnInfo col) {
+		m_customColumns.add(col);
+		return this;
+	}
+	
+	/**
+	 * Adds the columns {@link #EXPANDSTATE}, {@link #POS}, {@link #NOTEID} and {@link #UNID}.
+	 * 
+	 * @return this table
+	 */
+	public NotesMarkdownTable addAllStandardColumns() {
+		addColumn(EXPANDSTATE);
+		addColumn(POS);
+		addColumn(NOTEID);
+		addColumn(UNID);
+		return this;
+	}
+	
+	/**
+	 * Adds all columns of the view
+	 * 
+	 * @return this table
+	 */
+	public NotesMarkdownTable addAllViewColumns() {
+		m_view
+				.getColumns()
+				.stream()
+				.map((col) -> { return col.getItemName(); } )
+				.forEach(this::addViewColumn);
+		
+		return this;
+	}
+	
+	/**
+	 * Adds a column of the view to the table
+	 * 
+	 * @param itemName programmatic item name
+	 * @return this table
+	 */
+	public NotesMarkdownTable addViewColumn(String itemName) {
+		return addViewColumn(itemName, 40);
+	}
+	
+	/**
+	 * Adds a column of the view to the table
+	 * 
+	 * @param itemName programmatic item name
+	 * @param width column width in characters
+	 * @return this table
+	 */
+	public NotesMarkdownTable addViewColumn(String itemName, int width) {
+		NotesViewColumn col = m_view.getColumns()
+		.stream()
+		.filter((currCol) -> { return itemName.equalsIgnoreCase(currCol.getItemName()); })
+		.findFirst()
+		.orElseThrow(() -> new IllegalArgumentException(MessageFormat.format("Column {0} not found", itemName)));
+		
+		String title = col.getTitle() + "(" + col.getItemName()+")";
+
+		return addColumn(title, width, (entry) -> {
+			Object val = entry.get(itemName);
+			return valueToString(val);
+			
+		});
+		
+	}
+	
+	/**
+	 * Prints the table header
+	 * 
+	 * @return this table
+	 */
 	public NotesMarkdownTable printHeader() {
 		try {
 			StringBuilder sbLine1 = new StringBuilder();
 			StringBuilder sbLine2 = new StringBuilder();
-
-			List<NotesViewColumn> cols = m_view.getColumns();
 
 			int idx=0;
 
 			sbLine1.append("| ");
 			sbLine2.append("| ");
 
-			if (m_withExpandState) {
-				sbLine1.append("  |");
-				sbLine2.append("- |");
-			}
 
-			if (m_withPosition) {
-				if (idx>0) {
-					//close column to the left
-					sbLine1.append(" |");
-					sbLine2.append(" |");
-				}
-				int colWidth = getPositionColumnWidth();
-				sbLine1.append(" ").append(format("#", colWidth));
-				sbLine2.append(" ").append(StringUtil.repeat('-', colWidth));
-
-				idx++;
-			}
-
-			if (m_withNoteId) {
-				if (idx>0) {
-					//close column to the left
-					sbLine1.append(" |");
-					sbLine2.append(" |");
-				}
-				sbLine1.append(" ").append(format("NoteID", 12));
-				sbLine2.append(" ").append(StringUtil.repeat('-', 12));
-
-				idx++;
-			}
-
-			if (m_withUNID) {
-				if (idx>0) {
-					//close column to the left
-					sbLine1.append(" |");
-					sbLine2.append(" |");
-				}
-				sbLine1.append(" ").append(format("UNID", 32));
-				sbLine2.append(" ").append(StringUtil.repeat('-', 32));
-
-				idx++;
-			}
-
-			for (int i=0; i<cols.size(); i++) {
-				NotesViewColumn currCol = cols.get(i);
-				Integer colWidth = m_visibleColumnWidths.get(currCol);
-				if (colWidth==null) {
-					continue;
-				}
+			for (ColumnInfo currCol : m_customColumns) {
+				String colTitle = currCol.getTitle();
+				int colWidth = currCol.getWidth();
 
 				if (idx>0) {
 					//close column to the left
 					sbLine1.append(" |");
 					sbLine2.append(" |");
 				}
-
-				String title = currCol.getTitle() + "(" + currCol.getItemName()+")";
-				sbLine1.append(" ").append(format(title, colWidth));
+				sbLine1.append(" ").append(format(colTitle, colWidth));
 				sbLine2.append(" ").append(StringUtil.repeat('-', colWidth));
 
 				idx++;
@@ -264,89 +254,35 @@ public class NotesMarkdownTable {
 		entries.forEach(this::printRow);
 		return this;
 	}
-	
+
 	public NotesMarkdownTable printRows(Collection<NotesViewEntryData> entries) {
 		for (NotesViewEntryData currEntry : entries) {
 			printRow(currEntry);
 		}
 		return this;
 	}
-	
+
 	public NotesMarkdownTable printRow(NotesViewEntryData entry) {
 		try {
 			entry.setPreferNotesTimeDates(true);
 
-			List<NotesViewColumn> cols = m_view.getColumns();
-
 			int idx=0;
 
 			m_writer.write("| ");
-
-			if (m_withExpandState) {
-				ExpandState expandState = getExpandState(entry);
-				switch (expandState) {
-				case COLLAPSED:
-					m_writer.write("+");
-					break;
-				case EXPANDED:
-					m_writer.write("-");
-					break;
-				case NONE:
-					m_writer.write(" ");
-				default:
-				}
-				idx++;
-			}
-
-			if (m_withPosition) {
-				if (idx>0) {
-					//close column to the left
-					m_writer.write(" |");
-				}
-				int colWidth = getPositionColumnWidth();
-				m_writer.write(" ");
-				m_writer.write(format(entry.getPositionStr(), colWidth));
-				idx++;
-			}
-
-			if (m_withNoteId) {
-				if (idx>0) {
-					//close column to the left
-					m_writer.write(" |");
-				}
-				m_writer.write(" ");
-				m_writer.write(format(Integer.toString(entry.getNoteId()), 12));
-				idx++;
-			}
-
-			if (m_withUNID) {
-				if (idx>0) {
-					//close column to the left
-					m_writer.write(" |");
-				}
-				m_writer.write(" ");
-				m_writer.write(format(entry.getUNID().replace("00000000000000000000000000000000", ""), 32));
-
-				idx++;
-			}
-
-			for (int i=0; i<cols.size(); i++) {
-				NotesViewColumn currCol = cols.get(i);
-				Integer colWidth = m_visibleColumnWidths.get(currCol);
-				if (colWidth==null) {
-					continue;
+			
+			for (ColumnInfo currCol : m_customColumns) {
+				int colWidth = currCol.getWidth();
+				String colValue = currCol.getFunction().apply(entry);
+				if (colValue==null) {
+					colValue = "";
 				}
 
 				if (idx>0) {
 					//close column to the left
 					m_writer.write(" |");
 				}
-
-				String itemName = currCol.getItemName();
-				Object val = entry.get(itemName);
-				String sVal = valueToString(val);
 				m_writer.write(" ");
-				m_writer.write(format(sVal, colWidth));
+				m_writer.write(format(colValue, colWidth));
 
 				idx++;
 			}
@@ -371,7 +307,7 @@ public class NotesMarkdownTable {
 			throw new UncheckedIOException(e);
 		}
 	}
-	
+
 	private String valueToString(Object val) {
 		String sVal;
 		{
@@ -403,17 +339,92 @@ public class NotesMarkdownTable {
 			throw new UncheckedIOException(e);
 		}
 	}
-	
+
 	public int getDocEntryCount() {
 		return m_docEntryCount;
 	}
-	
+
 	public int getCategoryEntryCount() {
 		return m_categoryEntryCount;
 	}
-	
+
 	public int getTotalEntryCount() {
 		return m_totalEntryCount;
 	}
+
+	/**
+	 * Table column for the note id of the view entry
+	 */
+	public static final ColumnInfo NOTEID = new ColumnInfo("NoteID", 12, (entry) -> {
+		return Integer.toString(entry.getNoteId());
+	});
+
+	/**
+	 * Table column for the UNID of the view entry
+	 */
+	public static final ColumnInfo UNID = new ColumnInfo("UNID", 32, (entry) -> {
+		return entry.getUNID().replace("00000000000000000000000000000000", "");
+	});
+
+	/**
+	 * Table column for the COLLECTIONPOSITION of the view entry
+	 */
+	public static final ColumnInfo POS = new ColumnInfo("#", 20, (entry) -> { return entry.getPositionStr();});
+
+	private static enum ExpandState {NONE, EXPANDED, COLLAPSED};
+
+	/**
+	 * Table column for the exand state of the view entry. Returns an empty string for
+	 * entries with childCount==0, a "+" for collapsed entries with children and
+	 * "-" for expanded entries with children.
+	 */
+	public static final ColumnInfo EXPANDSTATE = new ColumnInfo("", 1,
+			new Function<NotesViewEntryData,String>() {
+
+		private Boolean m_collapsedListInverted;
+		private NotesIDTable m_collapsedList;
+
+		private ExpandState getExpandState(NotesViewEntryData entry) {
+			if (m_collapsedList==null) {
+				m_collapsedList = entry.getParent().getCollapsedList();
+			}
+			if (m_collapsedListInverted==null) {
+				m_collapsedListInverted = m_collapsedList.isInverted();
+			}
+
+			int noteId = entry.getNoteId();
+			if (noteId==0) {
+				return ExpandState.NONE;
+			}
+
+			int childCount = entry.getChildCount();
+			if (childCount==0) {
+				return ExpandState.NONE;
+			}
+
+			if (m_collapsedListInverted) {
+				//expand list
+				return m_collapsedList.contains(noteId) ? ExpandState.EXPANDED : ExpandState.COLLAPSED;
+			}
+			else {
+				return m_collapsedList.contains(noteId) ? ExpandState.COLLAPSED : ExpandState.EXPANDED;
+			}
+		}
+
+		@Override
+		public String apply(NotesViewEntryData entry) {
+			ExpandState expandState = getExpandState(entry);
+			switch (expandState) {
+			case COLLAPSED:
+				return "+";
+			case EXPANDED:
+				return "-";
+			case NONE:
+			default:
+				return " ";
+			}
+		}
+
+	});
 
 }
